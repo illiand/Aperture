14f99fe69e2b0dc6aa5fed93d65cb28b73035854
Ili <sion9801@gmail.com>
2022-10-11 17:08:40 -0400

    operation

---
+++ Aperture/Assets/Script/MainCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject character;
+    private float cameraDistance = -3f;
+
+    private float x = 0;
+    private float y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      float curDegree = transform.localEulerAngles.x;
+
+      transform.position = character.transform.position;
+      transform.localEulerAngles = character.transform.localEulerAngles;
+      x -= Input.GetAxis("Mouse Y");
+      x = Mathf.Clamp(x, -30, 40);
+      y += Input.GetAxis("Mouse X");
+
+      transform.localEulerAngles = new Vector3(x, y, 0);
+      character.transform.localEulerAngles = new Vector3(0, y, 0);
+      transform.position = character.transform.position;
+    }
+}
