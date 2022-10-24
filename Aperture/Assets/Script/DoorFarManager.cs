eed424c21d63de86978e08da3d4a17ace6d861b3
Ili <sion9801@gmail.com>
2022-10-24 02:50:31 -0400

    doorFar

---
+++ Aperture/Assets/Script/DoorFarManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorFarManager : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 pos;
+    private Vector3 targetPos;
+
+    private float curTime = 999;
+    private float time = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(curTime < time)
+      {
+        player.transform.position = Vector3.Lerp(pos, targetPos, curTime / time);
+
+        curTime += Time.deltaTime;
+      }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+          curTime = 0;
+
+          pos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+          targetPos = new Vector3(player.transform.position.x + 7f, player.transform.position.y, player.transform.position.z);
+        }
+    }
+
+    // void OnTriggerExit(Collider other)
+    // {
+    //     if(other.gameObject.name == "Player")
+    //     {
+    //       isIn = false;
+    //     }
+    // }
+}
